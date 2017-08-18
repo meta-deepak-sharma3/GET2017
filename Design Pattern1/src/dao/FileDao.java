89433a7205cfcbf3dae6a39cfd0a6f544163ab88
meta-deepak-sharma3 <deepak.sharma3@metacube.com>
2017-08-18 09:06:33 +0530

    Update FileDao.java

--- Design Pattern1/src/dao/FileDao.java
+++ Design Pattern1/src/dao/FileDao.java
@@ -9,6 +9,18 @@ import model.ProductVo;
  * For future use if the database is a file on a remote server.
  */
 public class FileDao implements BaseDao, ProductDao {
+	private BaseDao basedao = null;
+	
+	private FileDao(){}
+	
+	@Override
+	public static BaseDao getInstance(){
+		if(baseDao == null){
+			baseDao = new BaseDao();
+		}
+		return baseDao;
+	}
+	
 	@Override
 	public ProductVo getProduct(int productId) {
 		// TODO Auto-generated method stub
